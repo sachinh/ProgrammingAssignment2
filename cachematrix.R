46d0ac74c38732659d8c3dcffb6bc2a8e26524bc
Sachin Holla <nospamsachin@gmail.com>
2014-07-17 16:39:01 -0700

    Personalized Comments. Attempt at first commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## This is a set of functions to help with matrix inversion and to boot will implement caching for efficiency
+## function makeCacheMatrix: 
+##			this function will create a matrix and retrieve from cache if possible
+## function cacheSolve:
+##			this function will handle matrix inversion and retrieve the inverted matrix from cache, if possible
+##
+w
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
