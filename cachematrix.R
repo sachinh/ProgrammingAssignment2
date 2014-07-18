bc3f53e9e64effca40fbd4f447147dbe54cb9499
Sachin Holla <nospamsachin@gmail.com>
2014-07-17 17:04:40 -0700

    Coded Functions. No documentation yet.

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,37 @@
 ## function cacheSolve:
 ##			this function will handle matrix inversion and retrieve the inverted matrix from cache, if possible
 ##
-w
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
+}
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
