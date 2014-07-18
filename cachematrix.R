339765844595b550399fbe7f42818b67c5cb9463
Sachin Holla <nospamsachin@gmail.com>
2014-07-17 23:57:26 -0700

    Finalized testing of functions. Updated Documentation of functions

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,13 @@
 s_Square_Matrix_Allowed = "Inverting of non-square matrices is not allowed in this function."
 s_Cached_Matrix = "Getting cached data"
 
-## this function stores a cached matrix
+##
+## Function Name: makeCacheMatrix
+## Parameters   : Matrix (optional)
+## Purpose      : This allows to store, retrieve a matrix
+##              : and provides functions to set and get values easily
+## Developer    : Sachin Holla
+##
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -47,8 +53,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## this function returns the inverted matrix if its available.
-## if not, then inverts the stored matrix and caches this matrix.
+##
+## Function Name: cacheSolve
+## Parameters   : Matrix (mandatory)
+##              : and additional parameters (...)
+## Purpose      : This function returns the inverted matrix if its available.
+##              : If not, then does an explicit inversion on the matrix
+##              : on the stored matrix and returns this matrix while caching this matrix.
+## Developer    : Sachin Holla
+##
 
 cacheSolve <- function(x, ...) {
         
@@ -76,7 +89,14 @@ cacheSolve <- function(x, ...) {
         m
 }
 
-## Utility Function to check for a square matrix
+##
+## Function Name: isSquareMatrix
+## Parameters   : Matrix (optional)
+## Purpose      : Utility Function to check for a square matrix
+##              : Returns TRUE if the matrix passed is square (no. of rows = no. of columns)
+## Developer    : Sachin Holla
+##
+
 isSquareMatrix <- function(mat = matrix()) {
         ## now extract the number of rows and columns
         num_rows <- nrow(mat)
