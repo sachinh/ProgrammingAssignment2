6662d692fecfb320ff51470d100f87ff30ec94b7
Sachin Holla <nospamsachin@gmail.com>
2014-07-17 18:52:50 -0700

    Added utility func to handle non-square matrix and added additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,36 +5,88 @@
 ##			this function will handle matrix inversion and retrieve the inverted matrix from cache, if possible
 ##
 
-## Write a short comment describing this function
+## Declare String Literals for messages and such
+## Can help with Localization needs, if required
+s_Square_Matrix_Allowed = "Inverting of non-square matrices is not allowed in this function."
+s_Cached_Matrix = "Getting cached data"
+
+## this function stores a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
         m <- NULL
+
+        # Do the error check for non-Square Matrix
+        if ( !isSquareMatrix(x) ){
+                message(s_Square_Matrix_Allowed)
+                return (matrix(0))
+        }
+        
+        ## this function will allow to change the matrix after instantion
         set <- function(y) {
+                
+                # Do the error check for non-Square Matrix
+                if ( !isSquareMatrix(y) ){
+                        message(s_Square_Matrix_Allowed) 
+                        return ()
+                }
+                
+                # Valid matrix so go ahead with the 'setting' of data
                 x <<- y
                 m <<- NULL
         }
+        
+        # other functions: get, setsolve & getsolve are trivial
         get <- function() x
         setsolve <- function(solve) m <<- solve
         getsolve <- function() m
+        
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 
 }
 
-## Write a short comment describing this function
+## this function returns the inverted matrix if its available.
+## if not, then inverts the stored matrix and caches this matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-
+        
+        ## get the inverted matrix 
 	m <- x$getsolve()
+        
+        ## if the inverted matrix exists, then return this matrix with a message
         if(!is.null(m)) {
-                message("getting cached data")
+                message(s_Cached_Matrix)
                 return(m)
         }
+        
+        ## this means there is no stored inverted matrix so invert accordingly
         data <- x$get()
+        
+        ## error check for non-square matrix
+        if (!isSquareMatrix(data)) {
+                message(s_Square_Matrix_Allowed)
+                return (matrix())  
+        }
+        
+        ## now invert the matrix and return
         m <- solve(data, ...)
         x$setsolve(m)
         m
+}
+
+## Utility Function to check for a square matrix
+isSquareMatrix <- function(mat = matrix()) {
+        ## now extract the number of rows and columns
+        num_rows <- nrow(mat)
+        num_cols <- ncol(mat)
+        
+        # now check if rows are same as cols i.e. square matrix
+        matrix_is_square <- FALSE
+        if (num_rows == num_cols)
+                matrix_is_square <- TRUE
+        
+        matrix_is_square
+        
 }
